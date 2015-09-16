a778496cebf73710e355473b747e2a60a6e3eaa0
Mohammad Islam <reefat@gmail.com>
2015-09-15 23:11:58 -0400

    Use FastClick

--- src/app/config/run.js
+++ src/app/config/run.js
@@ -6,10 +6,8 @@
     .module('app')
     .run(runBlock);
 
-  function runBlock($log, $appInfo) {
-
-    //--> Do your magic here
-
+  function runBlock($window, $log, $appInfo) {
+    $window.FastClick.attach($window.document.body);
     $log.debug($appInfo.name + ' ' + $appInfo.version);
   }
 
