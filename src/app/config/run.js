903d65de317e32fbe3f18910e0544059f947ca2f
Mohammad Islam <reefat@gmail.com>
2015-09-22 00:28:35 -0400

    Add splash screen

--- src/app/config/run.js
+++ src/app/config/run.js
@@ -7,6 +7,16 @@
     .run(runBlock);
 
   function runBlock($window, $log, $appInfo) {
+    var document = $window.document,
+        navigator = $window.navigator;
+
+    document.addEventListener('deviceready', onDeviceReady, false);
+
+    // hide splash screen when device is ready
+    function onDeviceReady() {
+      navigator.splashscreen.hide();
+    }
+
     $log.debug($appInfo.name + ' ' + $appInfo.version);
   }
 
